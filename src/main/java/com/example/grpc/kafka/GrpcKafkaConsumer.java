cf56ac3d5a0fe6a1c2d95ff8972659b869c61d7c
Piyush Kumar Jayswal <PiyushJ004@github.com>
2024-03-08 12:32:44 +0530

    issue resolved for null property

--- src/main/java/com/example/grpc/kafka/GrpcKafkaConsumer.java
+++ src/main/java/com/example/grpc/kafka/GrpcKafkaConsumer.java
@@ -22,20 +22,20 @@ import lombok.extern.slf4j.Slf4j;
 public class GrpcKafkaConsumer {
 
 	@Autowired
-	private GrpcKafkaConsumerProperties kafkaProperties;
+	private GrpcKafkaConsumerProperties kafkaConsumerProperties;
 
 	@Bean
 	public Map<String, Object> kafkaConsumerConfig() {
-		log.info("-----Server URL : {}", kafkaProperties.getBootstrapServers());
+		log.info("-----Server URL : {}", kafkaConsumerProperties.getBootstrapServers());
 		Map<String, Object> properties = new LinkedHashMap<>();
-		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-		properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getGroupId());
-		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getAutoOffsetReset());
-		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getKeyDeserializer());
-		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaProperties.getValueDeserializer());
-		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaProperties.getEnableAutoCommit());
-		properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, kafkaProperties.getAutoCommitInterval());
-		properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaProperties.getSessionTimeout());
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConsumerProperties.getBootstrapServers());
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConsumerProperties.getGroupId());
+		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConsumerProperties.getAutoOffsetReset());
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaConsumerProperties.getKeyDeserializer());
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaConsumerProperties.getValueDeserializer());
+		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaConsumerProperties.getEnableAutoCommit());
+		properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, kafkaConsumerProperties.getAutoCommitInterval());
+		properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaConsumerProperties.getSessionTimeout());
 		properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 		return properties;
 	}
@@ -49,7 +49,7 @@ public class GrpcKafkaConsumer {
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(payloadConsumerFactory());
 		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-		factory.setConcurrency(kafkaProperties.getConcurrency());
+		factory.setConcurrency(kafkaConsumerProperties.getConcurrency());
 		return factory;
 	}
 
--- src/main/java/com/example/grpc/kafka/GrpckafkaProducer.java
+++ src/main/java/com/example/grpc/kafka/GrpckafkaProducer.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -18,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class GrpckafkaProducer {
 
+	@Autowired
 	private GrpcKafkaProducerProperties kafkaProducerProperties;
 
 	@Bean
