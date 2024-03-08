0cb56ddcb6f44b60c8367abcf2999231464f7a71
Piyush Kumar Jayswal <PiyushJ004@github.com>
2024-03-08 13:16:21 +0530

    compiled the proto file, classes generated

--- src/main/java/com/example/grpc/test/KafkaGrpc.java
+++ src/main/java/com/example/grpc/test/KafkaGrpc.java
@@ -1,16 +1,8 @@
-//package com.example.grpc.test;
-//
-//import io.grpc.BindableService;
-//import io.grpc.ServerServiceDefinition;
-//import net.devh.boot.grpc.server.service.GrpcService;
-//
-//@GrpcService
-//public class KafkaGrpc implements BindableService {
-//
-//	@Override
-//	public ServerServiceDefinition bindService() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//}
+package com.example.grpc.test;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class KafkaGrpc {
+
+}
